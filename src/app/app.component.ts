35807d2f59c8c5ee3011604d41797dff5bb19d26
Lewis <lewisyoul@gmail.com>
2018-02-22 15:21:28 +0000

    implement sidebar functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,42 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Platform, MenuController, Nav } from 'ionic-angular';
+
 
 import { HomePage } from '../pages/home/home';
+import { SearchMoviesPage } from '../pages/search-movies/search-movies';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  @ViewChild(Nav) nav: Nav;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  rootPage:any = HomePage;
+  pages: Array<{title: string, component: any}>;
+
+  constructor(
+    public menu: MenuController,
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'Search', component: SearchMoviesPage }
+    ]
+
+  }
+
+  openPage = (page) => {
+    this.menu.close();
+    this.nav.setRoot(page.component);
   }
 }
-
