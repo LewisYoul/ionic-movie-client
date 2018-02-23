db224f5d18662887f21368eb79329c7188a006f5
Allan Wazacz <cazwazacz@gmail.com>
2018-02-23 15:53:34 +0000

    Create logOut function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,25 +42,16 @@ export class MyApp {
       { title: 'Search', component: SearchMoviesPage },
       { title: 'Favourites', component: FavouritesPage }
     ]
-
-   //  this.authToken.signIn({email: "user@example.com", password: "monkey67"}).subscribe(
-   //
-   //     res => {
-   // 
-   //       console.log('auth response:', res);
-   //       console.log('auth response headers: ', res.headers.toJSON()); //log the response header to show the auth token
-   //       console.log('auth response body:', res.json()); //log the response body to show the user
-   //     },
-   //
-   //     err => {
-   //       console.error('auth error:', err);
-   //     }
-   // )
-
   }
 
   openPage = (page) => {
     this.menu.close();
     this.nav.setRoot(page.component);
+  }
+
+  logOut = () => {
+    this.authToken.signOut();
+    this.menu.close();
+    this.nav.setRoot(HomePage);
   }
 }
